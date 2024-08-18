5e6c1560fc62b01bccad722723ea1f4e28b3da99
firatkaanbitmez <firatbitmez@gmail.com>
2024-08-18 12:24:36 +0300

    basically in working

--- src/background.js
+++ src/background.js
@@ -1,10 +1,4 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.get({ categories: [] }, (data) => {
-    if (!data.categories.length) {
-      chrome.storage.sync.set({ categories: [] });
-    }
-  });
-});
+
 
 chrome.management.onEnabled.addListener((extension) => {
   console.log(`${extension.name} is enabled.`);
